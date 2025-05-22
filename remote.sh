c99dece89a0ba5c3f085cd3aeec54397da7874f8
Leon <132808925+Leon8326@users.noreply.github.com>
2025-05-22 19:14:04 +0200

    Update remote.sh

--- remote.sh
+++ remote.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export REMOTE="https://github.com/NarpPorts"
-export REMOTE_NAME="Bitey Ports Remote"
+export REMOTE_NAME="Bitey Ports"
 export REMOTE_ICON="🧳"
 export REMOTE_DESCRIPTION="Ports of all kinds of software to Bitey."
