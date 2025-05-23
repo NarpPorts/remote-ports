9aa1d4d9aa4417c3972e929ebc8480c7a641236b
Leon <132808925+Leon8326@users.noreply.github.com>
2025-05-23 12:45:27 +0200

    Create install.sh

---
+++ project/install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Hi!
+# This is a remote package.
+# It is only providing a remote, so no installation steps are required.
+# This is just an install.sh made to identify this package as non-broken.
